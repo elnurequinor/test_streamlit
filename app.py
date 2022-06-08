57b98364dae77262ebbeafeeeb8007c6a474c02b
Elnur Aliyev <97555021+elnurequinor@users.noreply.github.com>
2022-06-08 13:30:46 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import home, data, model, eln # import your app modules here
+
+app = MultiApp()
+
+st.markdown("""
+# Multi-Page App
+
+This multi-page app is using the [streamlit-multiapps]
+""")
+
+# Add all your application here
+app.add_app("Home", home.app)
+app.add_app("Data", data.app)
+app.add_app("Model", model.app)
+
+app.add_app("Elnur", eln.app)
+# The main app
+app.run()
