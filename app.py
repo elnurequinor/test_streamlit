3fe14ecbd49eff27c479ae865480d2bd9e0c5390
Elnur Aliyev <ELNA@equinor.com>
2022-06-14 10:24:05 -0500

    updated app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from multiapp import MultiApp
-from apps import home, data, model, eln # import your app modules here
+#from multiapp import MultiApp
+#from apps import home, data, model, eln # import your app modules here
 
-app = MultiApp()
+#app = MultiApp()
 
 st.markdown("""
 # Multi-Page App
@@ -11,10 +11,10 @@ This multi-page app is using the [streamlit-multiapps]
 """)
 
 # Add all your application here
-app.add_app("Home", home.app)
-app.add_app("Data", data.app)
-app.add_app("Model", model.app)
+#app.add_app("Home", home.app)
+#app.add_app("Data", data.app)
+#app.add_app("Model", model.app)#
 
-app.add_app("Elnur", eln.app)
-# The main app
-app.run()
+#app.add_app("Elnur", eln.app)
+## The main app
+#app.run()
